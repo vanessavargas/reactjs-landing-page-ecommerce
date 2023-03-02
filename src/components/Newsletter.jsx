96da4f70969d7d1d4f84da3d423c4c0a09b18863
Vanessa Vaegas <vanessa.vargas4f@gmail.com>
2023-03-02 19:22:17 -0300

    add alert Newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,21 +1,39 @@
 import React from "react";
+import swal from 'sweetalert';
 
-export default function Newsletter () {
-    return (
-        <div className="news">
-            <div id="content">
-                <h3>Cadastre-se e fique por dentro das novidades e promoções</h3>
-                <form id="form">
-                <input
-                    className="input"
-                    type="email"
-                    placeholder="E-mail"
-                    id="email"
-                    required
-                />
-                <input className="button-home" type="submit" value="ENVIAR" />
-                </form>
-            </div>
-        </div>
-    )
-}
+export default class Newsletter extends React.Component {
+            constructor(props) {
+              super(props);
+              this.state = {value: ''};
+          
+              this.handleChange = this.handleChange.bind(this);
+              this.handleSubmit = this.handleSubmit.bind(this);
+            }
+          
+            handleChange(event) {
+              this.setState({value: event.target.value});
+            }
+          
+            handleSubmit(event) {
+                swal('E-mail ' + this.state.value + ' cadastrado!');
+                event.preventDefault();
+            }
+            render() {
+              return (
+                <div className="news">
+                    <div id="content">
+                        <h3>Cadastre-se e fique por dentro das novidades e promoções</h3>
+                        <form onSubmit={this.handleSubmit}  id="form">
+                            <label>
+                                Name:
+                                <input className="input" type="email" value={this.state.value} onChange={this.handleChange} required/>
+                            </label>
+                            <input className="button-home"  type="submit" value="ENVIAR" />
+                        </form>
+                    </div>
+                </div>
+              );
+            }
+        }
+          
+    
